02a0437fe00419946efe3b6a2f9b847bc297a545
245965 <245965@edu.p.lodz.pl>
2024-03-02 18:28:07 +0100

    adding attributes(id,name,products), constructor,getters,setters

--- list/src/Category.java
+++ list/src/Category.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class Category {
+
+    private int id;
+    private String name;
+    private ArrayList<Product> products;
+
+    public Category(int id, String name, ArrayList<Product> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
 }
