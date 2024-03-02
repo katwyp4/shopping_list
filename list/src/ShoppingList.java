087fba5ef562a59965827ab018bcde134af47eb7
245965 <245965@edu.p.lodz.pl>
2024-03-02 20:25:12 +0100

    creating info method in ShoppingList - shows available actions

--- list/src/ShoppingList.java
+++ list/src/ShoppingList.java
@@ -2,4 +2,17 @@ public class ShoppingList {
     public static void main(String[] args) {
 
     }
+
+    private static void info(){
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("Available actions:");
+        System.out.println("1. Add product");
+        System.out.println("2. Show list");
+        System.out.println("3. Show list by category");
+        System.out.println("4. Remove list");
+        System.out.println("5. Remove list by category");
+        System.out.println("6. Remove product");
+        System.out.println("7. Save list");
+        System.out.println("-----------------------------------------------------------------------------------------");
+    }
 }
